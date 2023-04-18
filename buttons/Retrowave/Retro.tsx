41544271d97b1eb66ac0bc060c6bab7774629170
Tyler Cagle <tylerjcagle@gmail.com>
2023-04-18 11:31:46 -0400

    adjust css

--- buttons/Retrowave/Retro.tsx
+++ buttons/Retrowave/Retro.tsx
@@ -6,22 +6,12 @@ const Scene = () => {
   return (
     <div className={styles.scene}>
       <div className={styles.top}>
-        <div className={styles.startails}>
-          <div className={styles.startailR}></div>
-          <div className={styles.startailL}></div>
-          <div className={styles.startailM}></div>
-        </div>
-        <div className={styles.topLines}></div>
         <div className={styles.sun}></div>
         <div className={logo.container}>
           <div className={logo.triangle}></div>
-          <div className={logo.kodeText}></div>
         </div>
       </div>
       <div className={styles.bottom}>
-        <div className={styles.m0}></div>
-        <div className={styles.m1}></div>
-        <div className={styles.m2}></div>
         <div className={styles.bottomOverlay}></div>
       </div>
     </div>
