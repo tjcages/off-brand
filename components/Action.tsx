bef25b744757ccddbdec82b00d60a6ba1bf1103e
tylerj <tylerj@tylerjs-Air.lan>
2023-04-19 22:17:56 -0400

    update try it button

--- components/Action.tsx
+++ components/Action.tsx
@@ -1,16 +1,15 @@
+import Link from "next/link";
 import styles from "@/styles/nav.module.scss";
 
 const _ = () => {
   return (
-    <div className={styles.action}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <strong>Try it</strong>
-      </a>
-    </div>
+    <Link
+      className={styles.action}
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+      target="_blank"
+    >
+      <strong>Try it</strong>
+    </Link>
   );
 };
 
