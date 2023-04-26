fff6a99e9799de292125dfe2728ebc536ef7b455
Tyler J. Cagle <tylerjcagle@gmail.com>
2023-04-26 09:06:44 -0600

    fix twitter social

--- seo/index.tsx
+++ seo/index.tsx
@@ -40,7 +40,7 @@ const _ = ({
       <meta property="og:url" content={url} key="url" />
       <meta property="og:image" content={image} key="image" />
       {/* Twit */}
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="https://nategagnon.com" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:image" content={image} />
