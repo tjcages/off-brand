43f36dae2eacc804835f9cf536d33bab89ece76d
Tyler J. Cagle <tylerjcagle@gmail.com>
2023-04-26 09:08:01 -0600

    fix favicon

--- seo/index.tsx
+++ seo/index.tsx
@@ -14,21 +14,6 @@ const _ = ({
   image = "https://tryabuybutton.com/preview.jpg",
   url = "https://tryabuybutton.com",
 }: Props) => {
-  const [faviconHref, setFaviconHref] = useState("/favicon.ico");
-
-  useEffect(() => {
-    // Get current color scheme.
-    const matcher = window.matchMedia("(prefers-color-scheme: dark)");
-    // Set favicon initially.
-    setFaviconHref(getFaviconPath(matcher.matches));
-    // Change favicon if the color scheme changes.
-    matcher.onchange = () => setFaviconHref(getFaviconPath(matcher.matches));
-  }, [faviconHref]);
-
-  const getFaviconPath = (isDarkMode = false) => {
-    return isDarkMode ? "/favicon.ico" : "/favicon.ico";
-  };
-
   return (
     <Head>
       <title>{title}</title>
@@ -47,7 +32,7 @@ const _ = ({
       <meta name="twitter:description" content={description} />
       <meta name="twitter:creator" content="@NateyG" />
 
-      <link rel="icon" href={faviconHref} />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </Head>
   );
 };
