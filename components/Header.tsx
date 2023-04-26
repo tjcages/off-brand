f8b50ff4cd9851b9682a76d0c2d229d92acc2876
Tyler J. Cagle <tylerjcagle@gmail.com>
2023-04-26 09:01:59 -0600

    add scroll indicator

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,73 @@
 import Link from "next/link";
 import Image from "next/image";
+import { motion, Variants } from "framer-motion";
+import { useMedia, mobileBreakpoint } from "@/modules/useMedia";
 import styles from "@/styles/header.module.scss";
 
-const _ = () => {
-  return (
-    <div className={styles.main}>
-      {/* Stripe logo */}
-      <Link className={styles.logo} href="https://stripe.com" target="_blank">
-        <Image
-          src="/icons/stripe.svg"
-          alt="stripe logo"
-          width={60}
-          height={25}
-        />
-      </Link>
+const arrowVariants: Variants = {
+  offscreen: {
+    x: "-50%",
+    y: "200%",
+  },
+  onscreen: {
+    x: "-50%",
+    y: "0%",
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8,
+    },
+  },
+};
 
-      {/* Docs link */}
-      <Link
-        className={styles.action}
-        href="https://stripe.com/docs/payment-links/buy-button"
-        target="_blank"
+const _ = () => {
+  const mobile = useMedia(mobileBreakpoint);
+  return (
+    <>
+      {/* Nav */}
+      <motion.div
+        className={styles.main}
+        initial="offscreen"
+        whileInView="onscreen"
       >
-        <h5>Read the docs</h5>
-        <Image src="/icons/arrow.svg" alt="arrow icon" width={10} height={10} />
-      </Link>
-    </div>
+        {/* Stripe logo */}
+        <Link className={styles.logo} href="https://stripe.com" target="_blank">
+          <Image
+            src="/icons/stripe.svg"
+            alt="stripe logo"
+            width={60}
+            height={25}
+          />
+        </Link>
+
+        {/* Docs link */}
+        <Link
+          className={styles.action}
+          href="https://stripe.com/docs/payment-links/buy-button"
+          target="_blank"
+        >
+          <h5>Read the docs</h5>
+          <Image
+            src="/icons/arrow.svg"
+            alt="arrow icon"
+            width={10}
+            height={10}
+          />
+        </Link>
+
+        {/* Arrow */}
+        {mobile && (
+          <motion.div className={styles.arrow} variants={arrowVariants}>
+            <Image
+              src="/icons/arrow.svg"
+              alt="arrow icon"
+              width={100}
+              height={100}
+            />
+          </motion.div>
+        )}
+      </motion.div>
+    </>
   );
 };
 
