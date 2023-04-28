609a45530ec5274d1e862552ce4d6dc477ff0277
Tyler J. Cagle <tylerjcagle@gmail.com>
2023-04-28 13:15:37 -0600

    update

--- middleware.js
+++ middleware.js
@@ -10,7 +10,13 @@ const securityMiddleware = [
     // your CSP base configuration with IntelliSense
     // single quotes for values like 'self' are automatic
     directives: {
-      "img-src": ["self", "data:"],
+      "img-src": [
+        "self",
+        "data:",
+        "https://assets.codepen.io",
+        "https://i.postimg.cc",
+      ],
+      "style-src": ["self", "unsafe-inline"],
       "frame-src": ["self", "data:", "https://js.stripe.com"],
       "script-src": ["self", "https://js.stripe.com"],
     },
