eba04e46bcafb5ce833e018f7a32cf9ed10bfa3f
Tyler Cagle <tylerjcagle@gmail.com>
2023-07-25 11:23:04 -0600

    adjust parters

--- data/_partners.ts
+++ data/_partners.ts
@@ -13,38 +13,38 @@ const _ = [
   {
     name: "Nate Gagnon",
     position: "Creative Director",
-    href: "https://twitter.com/tjcages",
+    href: "https://twitter.com/NateyG",
   },
-  {
-    name: "Dylan Abruscato",
-    position: "Marketing & Partnerships",
-    href: "https://twitter.com/tjcages",
-  },
+  // {
+  //   name: "Dylan Abruscato",
+  //   position: "Marketing & Partnerships",
+  //   href: "https://twitter.com/tjcages",
+  // },
   {
     name: "Carter Strickling",
     position: "Product & Audio Designer",
-    href: "https://twitter.com/tjcages",
+    href: "https://twitter.com/Carterstrick96",
   },
   {
     name: "Aden Cesmat",
     position: "Sales & Partnerships",
-    href: "https://twitter.com/tjcages",
+    href: "https://www.linkedin.com/in/adencesmat",
   },
-  {
-    name: "Nam Hai",
-    position: "WebGL Engineer",
-    href: "https://twitter.com/tjcages",
-  },
-  {
-    name: "Thomas Hov",
-    position: "Art Director",
-    href: "https://twitter.com/tjcages",
-  },
-  {
-    name: "Andrew Ettinger",
-    position: "Copywriter",
-    href: "https://twitter.com/tjcages",
-  },
+  // {
+  //   name: "Nam Hai",
+  //   position: "WebGL Engineer",
+  //   href: "https://twitter.com/tjcages",
+  // },
+  // {
+  //   name: "Thomas Hov",
+  //   position: "Art Director",
+  //   href: "https://twitter.com/tjcages",
+  // },
+  // {
+  //   name: "Andrew Ettinger",
+  //   position: "Copywriter",
+  //   href: "https://twitter.com/tjcages",
+  // },
 ] as PartnerProps[];
 
 export default _;
