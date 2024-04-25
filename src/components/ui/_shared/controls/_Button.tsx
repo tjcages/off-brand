942ce57cc0e42e8e01ffe0f9b87d4f602ea6cb79
Tyler Cagle <tylerjcagle@gmail.com>
2024-04-25 09:18:51 -0700

    fix weird restart bug

--- src/components/ui/_shared/controls/_Button.tsx
+++ src/components/ui/_shared/controls/_Button.tsx
@@ -80,7 +80,7 @@ const _ = ({ step, next }: Props) => {
         <button
           id={id}
           className={cn(
-            "group w-12 md:w-16 h-12 md:h-16 p-0 rounded-full bg-white/10 backdrop-blur-lg",
+            "group w-12 md:w-16 h-12 md:h-16 p-0 rounded-full bg-white/10 backdrop-blur-md",
             Math.abs((selectedStep || 0) - step) > 1 && "opacity-50",
             Math.abs((selectedStep || 0) - step) > 2 && "opacity-25",
             Math.abs((selectedStep || 0) - step) > 3 && "opacity-10",
