913159bb9b446483315187b2e2af6c42468d4f51
Tyler Cagle <tylerjcagle@gmail.com>
2024-05-08 12:11:49 -0600

    clean packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { seo } from "@/seo";
 import { cn } from "@/utils";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata, Viewport } from "next";
-import PlausibleProvider from "next-plausible";
 
 import "@/styles/global.scss";
 
@@ -23,7 +22,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <body className={cn(Sohne.className, Sohne.variable)}>
-        <PlausibleProvider domain="beta.stripe.dev">{children}</PlausibleProvider>
+        {children}
         <Analytics />
       </body>
     </html>
