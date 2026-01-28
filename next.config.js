aa8c6d0c4237a73f953ba8ac5530ac0f56365d70
tylerj <tylerjcagle@gmail.com>
2026-01-27 18:46:16 -0800

    Add redirect for /privacy to Google Docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/privacy',
+        destination: 'https://docs.google.com/document/d/13fmzpHCpc3MnOUwLLd3jqOkYdHsSBy4tyXSht1RLwjE/edit?usp=drive_link',
+        permanent: false,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
